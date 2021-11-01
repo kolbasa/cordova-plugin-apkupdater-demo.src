3ac65aefd97df5501b70effa40377b0092f074f5
Michael Jedich <michael.jedich@pm.me>
2021-11-01 10:20:21 +0100

    Demo for v3.0.0.

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-function onDeviceReady(fn) {
-    document.addEventListener('deviceready', fn, false);
-}
-
-onDeviceReady(function() {
-    window.navigator.splashscreen.hide();
-})
-
 function addButtonClickListener(id, fn) {
     return document.getElementById(id).addEventListener('click', fn);
 }
-
-var initialHref = window.location.href;
-document.getElementById('reload-app').addEventListener('click', function () {
-    window.navigator.splashscreen.show();
-
-    setTimeout(function () {
-        window.location = initialHref;
-    }, 1000);
-});
-
